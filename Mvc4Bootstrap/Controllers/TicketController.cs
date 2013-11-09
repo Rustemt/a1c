2e30206be978d76fe74b9cfa509fe70756709f0b
rturkyilmaz <rturkyilmaz@rturkyilmaz-pc.nuevoyazilim.com>
2013-11-09 13:57:07 +0200

    srgsdg

---
+++ Mvc4Bootstrap/Controllers/TicketController.cs
@@ -0,0 +1,43 @@
+﻿using A1fxCrm.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace A1fxCrm.Web.Controllers
+{
+    public class TicketController : ApiController
+    {
+        private A1fxCrmEntities db = new A1fxCrmEntities();
+
+        [AllowAnonymous]
+        public object PostTicket(TicketModel loginModel)
+        {
+
+            CustomerTicket customerticket = new CustomerTicket
+            {
+                CustomerId = Convert.ToInt32(loginModel.id),
+                Content = loginModel.content,
+                CreatedDate = DateTime.Now,
+                Title = "Title"
+            };
+            db.CustomerTicket.Add(customerticket);
+            db.SaveChanges();
+            //if (LoginManager.Login(loginModel.username, loginModel.password, out errorMessage))
+            //{
+            //    return new { Result = true, User = LoginManager.CurrentUser.Transform() };
+            //}
+            //else
+            //{
+            return new { model = customerticket };
+            // }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Mvc4Bootstrap/Models/TicketModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A1fxCrm.Web.Models
+{
+    public class TicketModel
+    {
+        public string id { get; set; }
+        public string content { get; set; }
+    }
+}
